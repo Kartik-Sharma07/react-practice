50c003256e0ed073c75efcd87dc883385e33ab3d
Kartik <karthiksharma.16ec@saividya.ac.in>
2024-07-12 22:53:33 +0530

    Episode 03

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const heading = React.createElement("div", {id:"parent"}, 
-    React.createElement("div", {id:"child"}, [React.createElement("h1", {}, "hello"), React.createElement("h2", {}, "hello")]));
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(heading);
+/*const heading = React.createElement("div", {id:"parent"}, 
+    React.createElement("div", {id:"child"}, [React.createElement("h1", {}, "asd"), React.createElement("h2", {}, "dasd")]));
+*/
+
+const Content = () => (
+    <div id="content"> 
+        <h2 className="headers">This is content component</h2>
+    </div>
+);
+
+const title = (
+    <>
+        <h1 id="heading2" className="headers">This is the JSX title</h1>
+        {Content()}
+    </>
+);
+
+console.log(title);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 console.log(root);
 
-root.render(heading);
+root.render(title);
