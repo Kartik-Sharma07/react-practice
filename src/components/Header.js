b6b6b6f2c57e907b2fb557240b0b48b8274d9c77
Kartik <karthiksharma.16ec@saividya.ac.in>
2024-07-30 18:45:41 +0530

    Building Swiggy clone - part 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import { APP_LOGO_URL } from "../utils/constant";
+import { useState } from "react";
 
 const Header = () => {
+    const [btnText, setBtnText] = useState("login"); 
     return (
         <div className="header">
             <div className="logo-container">
@@ -12,6 +14,9 @@ const Header = () => {
                     <li>About us</li>
                     <li>Contact us</li>
                     <li>Cart</li>
+                    <button onClick={() => {
+                        setBtnText((btnText === "login") ? "logout" : "login") 
+                    }}>{btnText}</button>
                 </ul>
             </div>
         </div>
